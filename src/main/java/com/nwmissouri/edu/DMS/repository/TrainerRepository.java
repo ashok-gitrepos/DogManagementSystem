c306ae711d9de066d1dbcd46d12c36c03357bce3
ashok-gitrepos <ashok.chirumamilla@yahoo.com>
2023-04-29 18:05:28 -0500

    database

--- src/main/java/com/nwmissouri/edu/DMS/repository/TrainerRepository.java
+++ src/main/java/com/nwmissouri/edu/DMS/repository/TrainerRepository.java
@@ -7,10 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.nwmissouri.edu.DMS.models.Trainer;
 
-/**
- * @author S555373
- *
- */
+
 public interface TrainerRepository extends CrudRepository<Trainer, Integer>  {
 
 }
